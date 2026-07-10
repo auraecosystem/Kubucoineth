b13f91b8c3bf414f51d39be92febe3e125bb033f
Seriki Walter Yakub <webapp4@outlook.com>
2026-07-10 15:49:46 +0100

    Update and rename bin/bash.sh to bin/Vm/bash.zsh

--- bin/Vm/bash.zsh
+++ bin/Vm/bash.zsh
@@ -3,3 +3,11 @@ vyper --optimize gas MyContract.vy
 vyper --optimize gas MyContract.vy
 vyper -f bytecode MyContract.vy
 pip install vyper
+# Bytecode
+vyper -f bytecode Token.vy
+
+# ABI
+vyper -f abi Token.vy
+
+# Both
+vyper -f abi,bytecode Token.vy
